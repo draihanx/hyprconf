2b9371bcf74f11518f70fa9fabf2dd97077db005
Raihan <114208911+draihanx@users.noreply.github.com>
2024-07-05 08:15:29 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -245,7 +245,7 @@ if [[ -d "$HOME/.config/hypr/Wallpaper" ]]; then
 
 # setting the default wallpaper
   ln -sf "$wallpaper" "$HOME/.config/hypr/.cache/current_wallpaper.png"
-   echo "wallpaper = ,/home/raihan/.config/hypr/.cache/current_wallpaper.png" > "$HOME/.config/hypr/hyprpaper"
+   echo "wallpaper = ,/home/me-js-bro/.config/hypr/.cache/current_wallpaper.png" > "$HOME/.config/hypr/hyprpaper"
   "$HOME/.config/hypr/scripts/pywal.sh"
 fi
 
