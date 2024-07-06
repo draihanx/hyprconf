afe9ffcb5654b811179116a13dd7d69e5cca03ba
Raihan <114208911+draihanx@users.noreply.github.com>
2024-07-06 03:14:53 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -235,13 +235,13 @@ check_distro &> /dev/null
 
 if [[ -d "$HOME/.config/hypr/Wallpaper" ]]; then
   mode_file="$HOME/.mode"
-  #engine="$HOME/.config/hypr/.cache/.engine"
+  engine="$HOME/.config/hypr/.cache/.engine"
 
   touch "$mode_file" &> /dev/null
-  #touch "$engine" &> /dev/null
+  touch "$engine" &> /dev/null && echo "Engine file created!" && sleep 3
   
   echo "dark" > "$mode_file"
-  #echo "hyprpaper" > "$engine"
+  echo "hyprpaper" > "$engine"
 
   wallpaper="$HOME/.config/hypr/Wallpaper/$distro.png"
 
